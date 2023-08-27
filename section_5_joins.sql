5f4adbd74d60a487fe8c1512716e76b6222364b7
PabloVitorr <pablovitorstds@gmail.com>
2023-08-27 18:44:15 -0300

    Adicionando exercicios praticados

--- section_5_joins.sql
+++ section_5_joins.sql
@@ -41,3 +41,49 @@ SELECT
 FROM customer;
 
 -- LEFT OUTER JOIN --------------------------------------------------
+SELECT * FROM film;
+SELECT * FROM inventory;
+
+SELECT 
+	film.film_id, 
+	film.title, 
+	inventory.inventory_id,
+	inventory.store_id
+FROM film 
+	LEFT JOIN inventory ON inventory.film_id = film.film_id
+	WHERE inventory.film_id IS NULL;
+
+-- RIGHT JOINS ------------------------------------------------------
+SELECT 
+	film.film_id, 
+	film.title,
+	inventory.inventory_id,
+	inventory.store_id
+FROM film
+	RIGHT JOIN inventory ON inventory.film_id = film.film_id
+	WHERE film.film_id IS NULL;
+
+-- JOIN Challege ----------------------------------------------------
+SELECT * FROM customer;
+SELECT * FROM address;
+
+SELECT 
+	address.district,
+	customer.email
+FROM customer 
+	JOIN address ON customer.address_id = address.address_id
+	WHERE address.district = 'California';
+
+SELECT * FROM film;
+SELECT * FROM actor;
+SELECT * FROM film_actor;
+
+SELECT 
+	film.title,
+	actor.first_name,
+	actor.last_name
+FROM film_actor 
+	JOIN actor ON film_actor.actor_id = actor.actor_id
+	JOIN film ON film_actor.film_id = film.film_id
+	WHERE actor.first_name = 'Nick'
+	AND actor.last_name = 'Wahlberg';
